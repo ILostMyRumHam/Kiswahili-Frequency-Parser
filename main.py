8e20c6a9eae4072def505c36eb651739b3ce7a24
JISKRA <152342137+ILostMyRumHam@users.noreply.github.com>
2025-08-12 03:59:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import readjson
 
 def main():
-    '''Grab a random page of the parsed pdf from the json file and print the contents
+    '''Select a page from the json file and print the contents
     in the form of formatted rows of python dictionaries'''
     if __name__ == "__main__":
         print("---"*15)
         print("This is a parser for Swahili words")
+main()
     
